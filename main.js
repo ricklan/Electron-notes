65823a9b7feed4121852768e413196dea559dcee
Rick Lan <ricklan_blue@hotmail.com>
2022-04-10 18:07:27 -0400

    Added electron notes website

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, Tray, Menu } = require("electron");
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    title: "UTSC Notes",
+  });
+
+  mainWindow.maximize();
+  mainWindow.show();
+
+  mainWindow.on("minimize", function (event) {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on("close", function (event) {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+
+    return false;
+  });
+
+  mainWindow.loadURL("https://rlqyl.github.io/");
+
+  let tray = new Tray("./pics/notes_icon.jpg");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show App",
+      click: () => {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Quit",
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+});
